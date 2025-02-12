b4a536213c01e5f244430f5300eefe6aed134016
bowman-crego <bowmanccrego@gmail.com>
2025-02-12 12:46:08 -0800

    fixed some bugs

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -24,7 +24,7 @@ const Navbar = () => {
       <ul>
       {
         !loginCheck ? (
-          
+        
           <li className='nav-item button'>
             <button type='button'>
               <Link to='/login'>Login</Link>
